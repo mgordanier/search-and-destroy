fae91242ee2214d0a8390ad7e7eed94abb7a8781
Mary Gordanier <mcgordanier@gmail.com>
2020-05-28 23:57:14 -0400

    binarySearch interative solution

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,22 +1,24 @@
 'use strict'
 
 // Complete this algo
-const binarySearch = (array, target) => {
-  if (array.length < 2) {
-    return array[0] === target
-  }
+// const binarySearch = (array, target) => {
+//   if (array.length < 2) {
+//     return array[0] === target
+//   }
 
-  const middleIdx = Math.floor(array.length / 2)
-  const middleValue = array[middleIdx]
+//   const middleIdx = Math.floor(array.length / 2)
+//   const middleValue = array[middleIdx]
 
-  if (target === middleValue) {
-    return true
-  } else if (target < middleValue) {
-    return binarySearch(array.slice(0, middleIdx), target)
-  } else {
-    return binarySearch(array.slice(middleIdx + 1), target)
-  }
+//   if (target === middleValue) {
+//     return true
+//   } else if (target < middleValue) {
+//     return binarySearch(array.slice(0, middleIdx), target)
+//   } else {
+//     return binarySearch(array.slice(middleIdx + 1), target)
+//   }
 }
+
+
 
 /*
 	EXTRA CREDIT:
@@ -26,5 +28,23 @@ const binarySearch = (array, target) => {
 	How can we use other pieces of data, like pointers, to avoid slicing?
 
 */
+
+const binarySearch = (array, target) => { 
+  let startPointer = 0 
+  let endPointer = array.length - 1 
+  while (endPointer >=  startPointer) { 
+    let middleIdx = startPointer + Math.floor((endPointer - startPointer) / 2)
+    let middleValue = array[middleIdx] 
+    if (target === middleValue) { 
+      return true
+    } 
+    if (target > middleValue) { 
+      startPointer = middleIdx + 1
+    } else {
+      endPointer = middleIdx - 1 
+    }
+  }
+  return false
+}
 
 module.exports = binarySearch
